6e79882c05d8c7fdf6eba6a1f64a499ad7765026
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-02-07 22:23:21 +0800

    nvim: Add JSON language server with common schemas

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -8,6 +8,39 @@ local servers = {
   html = {},
   cssls = {},
   ts_ls = {},
+  jsonls = {
+    settings = {
+      json = {
+        -- Schemas https://www.schemastore.org
+        schemas = {
+          {
+            fileMatch = { "package.json" },
+            url = "https://json.schemastore.org/package.json",
+          },
+          {
+            fileMatch = { "tsconfig.json" },
+            url = "https://json.schemastore.org/tsconfig.json",
+          },
+          {
+            fileMatch = { ".prettierrc", ".prettierrc.json", "pretter.config.json" },
+            url = "https://json.schemastore.org/prettierrc.json",
+          },
+          {
+            fileMatch = { ".eslintrc", ".eslintrc.json" },
+            url = "https://json.schemastore.org/eslintrc.json",
+          },
+          {
+            fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+            url = "https://json.schemastore.org/babelrc.json",
+          },
+          {
+            fileMatch = { ".stylelintrc", ".stylelintrc.json", "stylelint.config.json" },
+            url = "https://json.schemastore.org/stylelintrc.json",
+          },
+        },
+      },
+    },
+  },
 }
 
 -- lsps with default config
