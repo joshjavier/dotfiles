3edc8a44233b656308a00a5ed9bbe457309fdf54
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-09-22 06:55:48 +0800

    Add linting to neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -655,6 +655,7 @@ require('lazy').setup({
         'stylua', -- Used to format Lua code
         'isort',
         'black', -- Used to format Python code
+        'eslint_d',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -925,7 +926,7 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
